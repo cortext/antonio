4937bdbd09a7459200935c69aafc4f0c20ceb049
c24b <4barbes@gmail.com>
2015-03-31 11:08:09 +0200

    DB test

--- antonio.py
+++ antonio.py
@@ -11,7 +11,9 @@ from sql2json import SQLConverter
 def load_sqldb(path_db=None, id_db=None):
 	'''From crawtext to Antonio: Load db from path_db and id'''
 	#full_db_path = os.path.join(path_db, id_db)
-	db = SQLConverter("cop-clean.db")
+	db = SQLConverter("./cop-clean.db")
 	db.list_tables()
+	db.get_data()
+	db.convert()	
 
 load_sqldb()
